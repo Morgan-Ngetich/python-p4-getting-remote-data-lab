a8a98ced08d1765505bcf22bbd9d479439feb9df
Morgan-Ngetich <ngetichmorgan6@gmail.com>
2024-01-26 16:04:49 +0300

    Completed

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,27 @@
-import requests
+import requests  # Popular library for making HTTP requests
 import json
 
 class GetRequester:
 
     def __init__(self, url):
-        self.url = url
+        self.url = url  # Initialize an instance variable
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)  # Use the 'requests.get' function to send a GET request to the URL
+        return response
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()  # calling the previous method
+        return response_body.json()  # Convert the response content to JSON
+        
+        
+
+# if __name__ == "__main__":
+url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+requester = GetRequester(url)
+
+response_body = requester.get_response_body()
+print(response_body)
+
+json_data = requester.load_json()
+print(json_data)
